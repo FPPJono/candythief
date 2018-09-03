37f8c055f2c3632e3b3fca5f1a83b1981deb426b
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-09-03 21:29:38 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -232,7 +232,7 @@ async function useEvidence(channel, roleid, emoji, reaction, user, message, evid
         if (member.roles.has(roleid)) {
             return
         }else {
-            user.send('please send the evidence id that you would like to try use on this.').then((newmsg) => {
+            user.send('please send the evidence id that you would like to try use on this.').then(async function(newmsg){
                 var input = await newmsg.channel.awaitMessages(response => response.author.id === user.id, { time:60000})
                 console.log(input)
                 if (input.content.toLowerCase() === evidenceid) {
