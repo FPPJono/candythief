c9747acc0355fa2869543f997ebb7109593f49a6
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-09-06 17:01:44 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,6 +110,33 @@ async function scorecard(person, message, bot) {
     });
 }
 
+const events = {
+    MESSAGE_REACTION_ADD: 'messageReactionAdd',
+    MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
+};
+bot.on('raw', async event => {
+    // `event.t` is the raw event name
+    if (!events.hasOwnProperty(event.t)) return;
+
+    const { d: data } = event;
+    const user = bot.users.get(data.user_id);
+    const channel = bot.channels.get(data.channel_id) || await user.createDM();
+
+    // if the message is already in the cache, don't re-emit the event
+    if (channel.messages.has(data.message_id)) return;
+
+    // if you're on the master/v12 branch, use `channel.messages.fetch()`
+    const message = await channel.fetchMessage(data.message_id);
+
+    // custom emojis reactions are keyed in a `name:ID` format, while unicode emojis are keyed by names
+    // if you're on the master/v12 branch, custom emojis reactions are keyed by their ID
+    const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
+    const reaction = message.reactions.get(emojiKey);
+
+    bot.emit(events[event.t], reaction, user);
+});
+
+
 function testCommand(message) {
     if (message.author.id != testacc) {
         message.channel.send("``sorry that is being worked on``")
