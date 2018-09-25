d4e4cb708fe34a28aa806ef5256b30f1a1a6086b
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-09-25 18:30:21 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,16 +3,9 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const PREFIX = "!";
 const fs = require('fs')
-const PNG = require('pngjs')
 const request = require('request')
 const async = require("async")
-var gifFrames = require('gif-frames')
 var gameMessage = new Function('return true')
-const download = require('image-downloader')
-
-var PImage = require('pureimage');
-var img1 = PImage.make(500,500);
-var tmp = require('tmp')
 
 //Bot Code
 
@@ -61,49 +54,6 @@ function wait(ms) {
 function decimalToHexString(number) {
     if (number < 0) { number = 0xFFFFFFFF + number + 1 }
     return number.toString(16).toUpperCase();
-}
-
-async function scorecard(person, message, bot) {
-    if ((person.displayAvatarURL.includes("png"))||(person.displayAvatarURL.includes("jpg"))||(person.displayAvatarURL.includes("jpeg"))){
-        await download.image({url: person.displayAvatarURL, dest:`pfp.png`})
-    }else if(person.displayAvatarURL.includes("gif")){
-        await gifFrames({url:person.displayAvatarURL, frames:0, outputType: 'png'}).then(function(frameData){
-            frameData[0].getImage().pipe(fs.createWriteStream(`pfp.png`))
-        })
-    }
-    var size = (600 / person.username.length)
-    if (size > 50){
-        size = 50
-    }
-    /*PImage.decodePNGFromStream(fs.createReadStream(`winCard.png`)).then((img) => {
-        var img2 = PImage.make(750,500);
-        var c = img2.getContext('2d');
-        c.drawImage(img,
-            0, 0, img.width, img.height, // source dimensions
-            0, 0, 750, 500              // destination dimensions
-        );
-        var ctx = c
-        var fnt = PImage.registerFont('scorefont.ttf', 'Score Font')
-        fnt.load(() => {
-            ctx.fillStyle = '#000000';
-            ctx.font = `24pt 'Score Font'`;
-            ctx.fillText(`Detective:`, 383, 364);
-            ctx.font = `24pt 'Score Font'`;
-            ctx.fillText(`Time Taken: ${(message.createdTimestamp - bot.guilds.get('484293337323667467').member(person).joinedTimestamp).toString().slice(0,-3)} seconds`, 397, 448);
-            ctx.font = `${size}pt 'Score Font'`;
-            ctx.fillText(`${person.username}`, 393, 407);
-            PImage.decodePNGFromStream(fs.createReadStream(`pfp.png`)).then((pfp) => {
-                c.drawImage(pfp,
-                    0, 0, pfp.width, pfp.height,
-                    517, 67, 100, 100
-               )
-                PImage.encodePNGToStream(img2,fs.createWriteStream('score.png')).then(() => {
-                    person.send({files:[{attachment: 'score.png', name:'winner.png'}] })
-                    bot.guilds.get('484293337323667467').channels.get('485205101913571329').send(`<@${message.author.id}> has won! their time was: ${(message.createdTimestamp - bot.guilds.get('484293337323667467').member(person).joinedTimestamp).toString().slice(0,-3)} seconds`)
-                });
-            })
-        });
-    });*/
 }
 
 const events = {
@@ -337,7 +287,10 @@ async function accuse(emoji, reaction, user) {
                             return room.channel.send("`you have gotten at least one thing incorrect`")
                         }else if (chosenEvidence.includes("boombox screw") != true) {
                             return room.channel.send("`you have gotten at least one thing incorrect`")
-                        }else scorecard(answer3.first().author, answer3.first(), bot)
+                        }else {
+                            bot.guilds.get('484293337323667467').channels.get('485205101913571329').send(`<@${ev3Check.author.id}> has won! their time was: ${(message.createdTimestamp - bot.guilds.get('484293337323667467').member(person).joinedTimestamp).toString().slice(0,-3)} seconds`)
+                            ev3Check.author.send("you won teehee")
+                        }
                     })
                 })
             })
