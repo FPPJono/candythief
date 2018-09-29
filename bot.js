23992771dcdb6c99e383423d05f7ab8b9457ecd3
FPPJono <34591633+FPPJono@users.noreply.github.com>
2018-09-29 12:38:44 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -321,6 +321,7 @@ async function useEvidence(reaction, channel, user, guild, member){
     if (user.bot) return
     user.send('please send the evidence id that you would like to try use on this.\nyou have 30 seconds to respond').then(async function(newmsg){
         var input = await newmsg.channel.awaitMessages(response => response.author.id === user.id, {max:1, time:30000, errors:['time']})
+        if (input.isEmpty() === true) return newmsg.channel.send('`time ran out, you must enter a response`')
         var answer = input.first().content.toLowerCase()
         if (channel.id === bm428){
             evidenceCheck(member,answer,"cm_192","taco",testrole)
